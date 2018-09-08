e1aacd1fd8faaeee54d34c13272d1be1a416fe82
Sathya Srikanth <sathyasrikanth@MacSSS.local>
2018-09-08 16:45:08 -0700

    Completed SQL project

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -27,35 +27,78 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
+/* 
+    Tennis Court 1, Tennis Court 2, Massage Room 1, Massage Room 2, Squash Court
+*/
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+/*
+    4
+*/
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+/*
+
+SELECT facid, name, membercost, monthlymaintenance FROM `Facilities`
+WHERE membercost < .2 * monthlymaintenance
+
+*/
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
 
+/*
+
+SELECT * FROM `Facilities`
+WHERE facid IN (1,5)
+
+
+*/
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
 
+/*
+
+SELECT name, monthlymaintenance,
+CASE WHEN monthlymaintenance < 100 THEN 'cheap' ELSE 'expensive' END
+FROM `Facilities`
+
+*/
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
 
+/*
+
+SELECT surname, firstname 
+FROM `Members`
+WHERE joindate = (SELECT MAX(joindate) FROM `Members`)
+
+*/
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+/*
+
+SELECT DISTINCT concat(m.firstname, ' ', m.surname) memname, f.name facility
+FROM `Members` m
+INNER JOIN `Bookings` b ON (m.memid = b.memid)
+INNER JOIN `Facilities` f ON (b.facid = f.facid)
+WHERE f.name LIKE 'Tennis%'
+ORDER BY memname, facility
+
+*/
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30? Remember that guests have
@@ -64,10 +107,50 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+/*
+
+SELECT concat(m.firstname, ' ', m.surname) memname, f.name facility,
+       (CASE WHEN m.memid = 0 THEN f.guestcost * b.slots
+        ELSE f.membercost * b.slots END) cost
+FROM `Members` m
+INNER JOIN `Bookings` b ON (m.memid = b.memid)
+INNER JOIN `Facilities` f ON (b.facid = f.facid)
+WHERE (b.starttime LIKE '2012-09-14%') AND
+ ((m.memid = 0 AND b.slots * f.guestcost > 30) OR
+  (m.memid > 0 AND b.slots * f.membercost > 30))
+ORDER BY cost DESC
+
+*/
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+/*
+
+SELECT memname, facility, cost from (
+  SELECT concat(m.firstname, ' ', m.surname) memname, f.name as facility,
+  CASE WHEN m.memid = 0 THEN b.slots * f.guestcost
+  ELSE b.slots * f.membercost END cost
+  FROM `Members` AS m
+  INNER JOIN `Bookings` AS b ON m.memid = b.memid
+  INNER JOIN `Facilities` AS f ON b.facid = f.facid
+  WHERE b.starttime LIKE '2012-09-14%'
+) bookings
+WHERE cost > 30
+ORDER BY cost DESC
+
+*/
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
+
+/*
+
+SELECT f.name, SUM(b.slots * (CASE WHEN b.memid = 0 THEN f.guestcost ELSE f.membercost END)) revenue
+FROM `Bookings` b
+INNER JOIN `Facilities` f ON b.facid = f.facid
+GROUP BY f.name
+HAVING revenue < 1000
+ORDER BY revenue
+
+*/
